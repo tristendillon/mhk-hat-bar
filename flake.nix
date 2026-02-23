362528c7f3a8475f94a50b7aa4fc2a3362b38737
Tristen Dillon <tristendillon24@outlook.com>
2026-02-22 23:40:22 -0600

    start

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "MHK Hat Bar - Astro + React + Tailwind + Bun";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+            nodejs_22
+          ];
+        };
+      });
+}
